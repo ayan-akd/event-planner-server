827076f62af69711249a6ea8c5718a93615a6091
ayan-akd <ayankumar.akd@gmail.com>
2025-05-01 17:36:32 +0600

    FEAT: Setup eslint and update dependencies

--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -6,18 +6,11 @@ import httpStatus from "http-status";
 import { Prisma } from "@prisma/client";
 import { AppError } from "../errors/AppError";
 import handleZodError from "../errors/ZodError";
-import { TErrorSources } from "../errors/error.interface";
 import { ZodError } from "zod";
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   let statusCode = 500;
   let message = "Something went wrong!";
-  let errorSources: TErrorSources = [
-    {
-      path: "",
-      message: "Something went wrong",
-    },
-  ];
 
   if (err instanceof ZodError) {
     const simplifiedError = handleZodError(err);
